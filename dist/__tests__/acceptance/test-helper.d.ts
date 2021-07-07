fb684225fad45cb236c79da6cd13570687569ad7
Laptop-Nhan <hp.duongducnhan@gmail.com>
2021-07-07 16:36:39 +0700

    add dist

---
+++ dist/__tests__/acceptance/test-helper.d.ts
@@ -0,0 +1,7 @@
+import { TestApplication } from '../..';
+import { Client } from '@loopback/testlab';
+export declare function setupApplication(): Promise<AppWithClient>;
+export interface AppWithClient {
+    app: TestApplication;
+    client: Client;
+}
